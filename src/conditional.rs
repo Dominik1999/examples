783e4a84d8f72ca620d342c43434d9737162a2e2
Bobbin Threadbare <43513081+bobbinth@users.noreply.github.com>
2022-10-07 08:30:16 -0700

    Initial commit

---
+++ src/conditional.rs
@@ -0,0 +1,63 @@
+use super::*;
+use log::debug;
+
+// EXAMPLE BUILDER
+// ================================================================================================
+
+pub fn get_example(flag: usize) -> Example {
+    // convert flag to a field element
+    let flag = flag as u64;
+
+    // determine the expected result
+    let expected_result = match flag {
+        0 => 15u64,
+        1 => 8u64,
+        _ => panic!("flag must be a binary value"),
+    };
+
+    // construct the program which either adds or multiplies two numbers
+    // based on the value provided via secret inputs
+    let assembler = Assembler::new(true);
+    let program = assembler.compile(
+        "
+    begin
+        push.3
+        push.5
+        read
+        if.true
+            add
+        else
+            mul
+        end
+    end",
+    )
+    .unwrap();
+
+    debug!(
+        "Generated a program to test conditional execution; expected result: {}",
+        expected_result
+    );
+
+    Example {
+        program,
+        inputs: ProgramInputs::new(&[], &[flag], vec![]).unwrap(),
+        pub_inputs: vec![],
+        expected_result: vec![expected_result],
+        num_outputs: 1,
+    }
+}
+
+// EXAMPLE TESTER
+// ================================================================================================
+
+#[test]
+fn test_conditional_example() {
+    let example = get_example(1);
+    super::test_example(example, false);
+}
+
+#[test]
+fn test_conditional_example_fail() {
+    let example = get_example(1);
+    super::test_example(example, true);
+}
